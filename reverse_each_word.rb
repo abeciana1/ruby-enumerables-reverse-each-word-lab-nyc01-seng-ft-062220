2649028360569a7311f15bc286f9137a27702c6f
Alex Beciana <alex.beciana@gmail.com>
2020-06-14 20:13:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require 'pry'
 def reverse_each_word(sentence)
   initial = [sentence.split(" ")]
   results = []
-  binding.pry
   initial.each do |x|
+    binding.pry
     x.reverse << results
   end
 end
