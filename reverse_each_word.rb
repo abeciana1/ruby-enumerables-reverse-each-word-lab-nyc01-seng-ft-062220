119d4e279abe2748bdd6ec7491cb21dc56550037
Alex Beciana <alex.beciana@gmail.com>
2020-06-14 20:06:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,14 @@
 def reverse_each_word(sentence)
+  results = []
+  
   array[sentence].each do |x|
-    
+    x.reverse << results
+  end
 end
 
 
-array = ["the cow goes moo"]
+#array = ["the cow goes moo"]
 
-puts array.reverse
+#puts array.reverse
+
+reverse_each_word("the cow goes moo")
