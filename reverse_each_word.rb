4be784f0aff3718ed97f2c5c8c6773a62ddb2453
Alex Beciana <alex.beciana@gmail.com>
2020-06-14 20:57:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-def reverse_each_word(sentence)
+#def reverse_each_word(sentence)
   initial = sentence.reverse.split(" ")
   
   initial.reverse.join(" ")
-end
+# end
 
 #def reverse_each_word(sentence)
   initial = sentence.reverse.split(" ")
